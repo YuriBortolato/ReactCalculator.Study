be419bdf8ebc305ead1a185b8586db5e0cd03436
YuriBortolato <yurigbort@gmail.com>
2026-03-06 19:41:05 -0300

    commit inicial

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="Index" options={{ title="Home"}} />
+    <Stack.Screen name="about" options={{ title="Tela Dois"}} />
+    </Stack>
+}
---
+++ app/telaDois.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+
+export default function telaDois() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Tela Dois.</Text>
+    </View>
+  );
+}
