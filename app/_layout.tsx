a5ee258f3c73ab2604601e4c95d7b7dcd135e687
YuriBortolato <yurigbort@gmail.com>
2026-03-07 15:15:39 -0300

    feat: configura navegacao stack e cria tela inicial da calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,10 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="teladois" options={{ title: "TelaDois" }} />
+      <Stack.Screen name="soma" options={{ title: "Somar" }} />
+      <Stack.Screen name="subtracao" options={{ title: "Subtrair" }} />
+      <Stack.Screen name="multiplicacao" options={{ title: "Multiplicar" }} />
+      <Stack.Screen name="divisao" options={{ title: "Dividir" }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,43 @@
-import { Link, useNavigation } from "expo-router";
-import { Button, View } from "react-native";
-import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
-
-type Screens = {
-  teladois: undefined;
-};
-
-type NavigationProps = NativeStackNavigationProp<Screens>;
+import { useRouter } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  const navigation = useNavigation<NavigationProps>();
+  const router = useRouter();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        title="Navegar Botao"
-        onPress={() => navigation.navigate("teladois")}
-      />
-      <Link href={"/teladois"}>Navegar</Link>
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculadora UMFG</Text>
+      
+      <View style={styles.buttonContainer}>
+        <Button title="Somar (+)" onPress={() => router.push("/soma")} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Subtrair (-)" onPress={() => router.push("/subtracao")} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Multiplicar (*)" onPress={() => router.push("/multiplicacao")} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Dividir (/)" onPress={() => router.push("/divisao")} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  buttonContainer: {
+    width: "100%",
+    marginBottom: 15,
+  }
+});
