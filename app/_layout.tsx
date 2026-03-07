6a8d445859a91c7b2d54b6eb041e51562acf8954
YuriBortolato <yurigbort@gmail.com>
2026-03-07 16:33:59 -0300

    fix: centraliza os titulos do cabecalho (header) em todas as telas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,12 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerTitleAlign: 'center' }}>
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="soma" options={{ title: "Somar" }} />
-      <Stack.Screen name="subtracao" options={{ title: "Subtrair" }} />
-      <Stack.Screen name="multiplicacao" options={{ title: "Multiplicar" }} />
-      <Stack.Screen name="divisao" options={{ title: "Dividir" }} />
+      <Stack.Screen name="soma" options={{ title: "Soma" }} />
+      <Stack.Screen name="subtracao" options={{ title: "Subtração" }} />
+      <Stack.Screen name="multiplicacao" options={{ title: "Multiplicação" }} />
+      <Stack.Screen name="divisao" options={{ title: "Divisão" }} />
     </Stack>
   );
 }
